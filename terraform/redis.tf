c14097d48cfab553a16ebb51bcbeea9459a4d1bb
Ventsislav Kaynakchiev <ventsislav.kaynakchiev@versata.com>
2017-08-13 22:29:34 +0300

    Redis deployment

---
+++ terraform/redis.tf
@@ -0,0 +1,10 @@
+module "redis" {
+  source         = "github.com/terraform-community-modules/tf_aws_elasticache_redis?ref=1.0.0"
+  env            = "${var.environment}"
+  name           = "devops-${var.environment}"
+  redis_clusters = "${var.environment == "production" ? 2 : 1}"
+  redis_failover = "true"
+  subnets        = "${module.vpc.database_subnets}"
+  vpc_id         = "${module.vpc.vpc_id}"
+  redis_node_type = "${var.environment == "production" ? "cache.m4.large" : "cache.t2.micro"}"
+}
