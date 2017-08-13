1de55928e27c3bf69764287248eae3d9e6d0905a
Ventsislav Kaynakchiev <ventsislav.kaynakchiev@versata.com>
2017-08-13 22:18:37 +0300

    Egress SG rule, SSH access for testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,23 @@ resource "aws_security_group" "app" {
     security_groups = ["${aws_security_group.elb.id}"]
   }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
   tags {
     Name = "${var.app_name}"
   }
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type            = "ingress"
+  from_port       = 22
+  to_port         = 22
+  protocol        = "tcp"
+  source_security_group_id = "${aws_security_group.app.id}"
+  security_group_id = "${aws_security_group.app.id}"
 }
