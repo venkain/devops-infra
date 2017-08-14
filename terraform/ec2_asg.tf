98a725f9dc9344f6e4a09f1dbb8965762bb09878
Ventsislav Kaynakchiev <ventsislav.kaynakchiev@versata.com>
2017-08-15 01:37:15 +0300

    SNS alerting

--- terraform/ec2_asg.tf
+++ terraform/ec2_asg.tf
@@ -20,35 +20,40 @@ data "template_file" "user_data" {
   template = "${file("user_data.sh")}"
 
   vars {
-    elb_url = "${aws_alb.app.dns_name}",
-    postgres_url = "${module.rds.rds_instance_address}",
-    db_name = "${var.database_name}",
-    gitlab_db_name = "${var.gitlab_db_name}",
-    db_user = "${var.database_user}",
-    db_password = "${var.database_password}",
-    redis_url = "${module.redis.endpoint}"
-  }
+    elb_url        = "${aws_alb.app.dns_name}"
+    postgres_url   = "${module.rds.rds_instance_address}"
+    db_name        = "${var.database_name}"
+    gitlab_db_name = "${var.gitlab_db_name}"
+    db_user        = "${var.database_user}"
+    db_password    = "${var.database_password}"
+    redis_url      = "${module.redis.endpoint}"
 
+    # TODO: fix EFS target
+    efs_url = "${aws_efs_mount_target.app_1.dns_name}"
+  }
 }
 
 resource "aws_launch_configuration" "app" {
-  name_prefix   = "terraform-lc-${var.app_name}-"
-  image_id      = "${data.aws_ami.ubuntu.id}"
-  instance_type = "${var.environment == "production" ? var.instance_type_prod : var.instance_type_dev}"
-  security_groups = [ "${aws_security_group.app.id}" ]
+  name_prefix     = "terraform-lc-${var.app_name}-"
+  image_id        = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "${var.environment == "production" ? var.instance_type_prod : var.instance_type_dev}"
+  security_groups = ["${aws_security_group.app.id}"]
 
   lifecycle {
     create_before_destroy = true
   }
-  // enable_monitoring = false
-#   iam_instance_profile =
-# Remove after test
+
+  enable_monitoring = false
+
+  #   iam_instance_profile =
+  # Remove after test
   key_name = "venkain"
+
   user_data = "${data.template_file.user_data.rendered}"
 }
 
 resource "aws_autoscaling_group" "app" {
-  availability_zones        = [ "${slice(data.aws_availability_zones.available.names, 0, 2)}" ]
+  availability_zones        = ["${slice(data.aws_availability_zones.available.names, 0, 2)}"]
   name                      = "${var.app_name}"
   max_size                  = "${var.max_size}"
   min_size                  = "${var.min_size}"
@@ -56,9 +61,10 @@ resource "aws_autoscaling_group" "app" {
   health_check_type         = "ELB"
   desired_capacity          = "${var.environment == "production" ? 2 : 1}"
   force_delete              = true
+
   # placement_group           = "${aws_placement_group.app.id}"
-  launch_configuration      = "${aws_launch_configuration.app.name}"
-  vpc_zone_identifier = [ "${module.vpc.private_subnets}" ]
+  launch_configuration = "${aws_launch_configuration.app.name}"
+  vpc_zone_identifier  = ["${module.vpc.private_subnets}"]
 
   lifecycle {
     create_before_destroy = true
@@ -69,20 +75,20 @@ resource "aws_autoscaling_group" "app" {
   }
 
   tag {
-    key = "Name"
-    value = "${var.app_name}"
+    key                 = "Name"
+    value               = "${var.app_name}"
     propagate_at_launch = true
   }
 
   tag {
-    key = "Terraform"
-    value = "true"
+    key                 = "Terraform"
+    value               = "true"
     propagate_at_launch = true
   }
 
   tag {
-    key = "Environment"
-    value = "${var.environment}"
+    key                 = "Environment"
+    value               = "${var.environment}"
     propagate_at_launch = true
   }
 }
@@ -90,4 +96,16 @@ resource "aws_autoscaling_group" "app" {
 resource "aws_autoscaling_attachment" "app" {
   autoscaling_group_name = "${aws_autoscaling_group.app.id}"
   alb_target_group_arn   = "${aws_alb_target_group.app.arn}"
+}
+
+resource "aws_autoscaling_notification" "email_alerts" {
+  group_names = ["${aws_autoscaling_group.app.name}"]
+
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+  ]
+
+  topic_arn = "${var.sns_alerts_arn}"
 }
