9dcade969d7d1a057f0727d7cdf395cb8a4aab48
Ventsislav Kaynakchiev <ventsislav.kaynakchiev@versata.com>
2017-08-15 01:36:04 +0300

    Added SNS alerts ARN

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,59 +1,64 @@
 variable "profile" {
-    description = "AWS user profile"
-    default = "venkain"
+  description = "AWS user profile"
+  default     = "venkain"
 }
 
 variable "region" {
-    description = "AWS region"
-    default = "us-east-1"
+  description = "AWS region"
+  default     = "us-east-1"
 }
 
 variable "environment" {
-    description = "Environment identifier"
-    default = "development"
+  description = "Environment identifier"
+  default     = "development"
 }
 
 variable "app_name" {
-    description = "Application name"
-    default = "gitlab"
+  description = "Application name"
+  default     = "gitlab"
 }
 
 variable "organization" {
-    description = "Organization"
-    default = "ACME Inc."
+  description = "Organization"
+  default     = "ACME Inc."
 }
 
 variable "country" {
-    description = "Organization's country"
-    default = "GB"
+  description = "Organization's country"
+  default     = "GB"
 }
 
 variable "domain" {
-    description = "Organization domain"
-    default = ""
+  description = "Organization domain"
+  default     = ""
 }
 
 variable "instance_type_prod" {
-    description = "Production instance type"
-    default = "m4.large"
+  description = "Production instance type"
+  default     = "m4.large"
 }
 
 variable "instance_type_dev" {
-    description = "Development instance type"
-    default = "t2.medium"
+  description = "Development instance type"
+  default     = "t2.medium"
 }
 
 variable "min_size" {
-    description = "Minimum autoscaling size"
-    default = 1
+  description = "Minimum autoscaling size"
+  default     = 1
 }
 
 variable "max_size" {
-    description = "Maximum autoscaling size"
-    default = 4
+  description = "Maximum autoscaling size"
+  default     = 4
 }
 
 variable "gitlab_db_name" {
-    description = "Gitlab DB name"
-    default = "gitlabdb"
+  description = "Gitlab DB name"
+  default     = "gitlabdb"
+}
+
+variable "sns_alerts_arn" {
+  description = "SNS topic ARN for autoscaling alerts."
+  default     = "arn:aws:sns:us-east-1:443499217059:Alerts"
 }
