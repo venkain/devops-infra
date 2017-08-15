aaaac7c71c18d7a58f617689d14ae3fd48e1b6b0
Ventsislav Kaynakchiev <ventsislav.kaynakchiev@versata.com>
2017-08-15 14:21:54 +0300

    Enabled metrics and fixed logic

--- terraform/ec2_asg.tf
+++ terraform/ec2_asg.tf
@@ -18,13 +18,15 @@ data "template_file" "user_data" {
   template = "${file("user_data.sh")}"
 
   vars {
-    elb_url        = "${aws_alb.app.dns_name}"
+    url            = "${null_resource.url.triggers.address}"
     postgres_url   = "${module.rds.rds_instance_address}"
     db_name        = "${var.database_name}"
     gitlab_db_name = "${var.gitlab_db_name}"
     db_user        = "${var.database_user}"
     db_password    = "${var.database_password}"
     redis_url      = "${module.redis.endpoint}"
+    s3_bucket_name = "${aws_s3_bucket.ssh_public_keys.id}"
+    s3_bucket_uri  = ""
 
     # TODO: fix EFS target
     efs_url = "${aws_efs_mount_target.app_1.dns_name}"
@@ -41,11 +43,6 @@ resource "aws_launch_configuration" "app" {
     create_before_destroy = true
   }
 
-  enable_monitoring = false
-
-  # Remove after test
-  # key_name = "xxx"
-
   user_data = "${data.template_file.user_data.rendered}"
 }
 
@@ -56,12 +53,23 @@ resource "aws_autoscaling_group" "app" {
   min_size                  = "${var.min_size}"
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = "${var.environment == "production" ? 2 : 1}"
+  desired_capacity          = "${var.min_size}"
   force_delete              = true
 
   # placement_group           = "${aws_placement_group.app.id}"
   launch_configuration = "${aws_launch_configuration.app.name}"
   vpc_zone_identifier  = ["${module.vpc.private_subnets}"]
+
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances",
+  ]
 
   lifecycle {
     create_before_destroy = true
@@ -96,7 +104,7 @@ resource "aws_autoscaling_attachment" "app" {
 }
 
 resource "aws_autoscaling_notification" "email_alerts" {
-  count = "${var.sns_alerts_arn == "" ? 0 : 1}"
+  count       = "${var.sns_alerts_arn == "" ? 0 : 1}"
   group_names = ["${aws_autoscaling_group.app.name}"]
 
   notifications = [
