a3741e4d74be855afc3b074a15f9289fbfc290e2
Ventsislav Kaynakchiev <ventsislav.kaynakchiev@versata.com>
2017-08-15 15:58:19 +0300

    Diabled deletion protection

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -4,7 +4,7 @@ resource "aws_alb" "app" {
   security_groups = ["${aws_security_group.elb.id}"]
   subnets         = ["${module.vpc.public_subnets}"]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   # access_logs {
   #   bucket = "${aws_s3_bucket.alb_logs.bucket}"
