94ab8432624e0641bc3da1d99d72e42bff59094a
Ventsislav Kaynakchiev <ventsislav.kaynakchiev@versata.com>
2017-08-15 14:20:07 +0300

    Formatting

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,12 +1,12 @@
 module "redis" {
-  source                  = "github.com/terraform-community-modules/tf_aws_elasticache_redis"
-  env                     = "${var.environment}"
-  name                    = "${var.app_name}-${var.environment}"
-  redis_clusters          = "${var.environment == "production" ? 2 : 1}"
-  redis_failover          = "${var.environment == "production" ? "true" : "false"}"
-  subnets                 = "${module.vpc.database_subnets}"
-  vpc_id                  = "${module.vpc.vpc_id}"
-  redis_node_type         = "${var.environment == "production" ? "cache.m4.large" : "cache.t2.micro"}"
+  source          = "github.com/terraform-community-modules/tf_aws_elasticache_redis"
+  env             = "${var.environment}"
+  name            = "${var.app_name}-${var.environment}"
+  redis_clusters  = "${var.environment == "production" ? 2 : 1}"
+  redis_failover  = "${var.environment == "production" ? "true" : "false"}"
+  subnets         = "${module.vpc.database_subnets}"
+  vpc_id          = "${module.vpc.vpc_id}"
+  redis_node_type = "${var.environment == "production" ? "cache.m4.large" : "cache.t2.micro"}"
 }
 
 resource "aws_security_group_rule" "redis" {
@@ -17,5 +17,3 @@ resource "aws_security_group_rule" "redis" {
   source_security_group_id = "${aws_security_group.app.id}"
   security_group_id        = "${module.redis.redis_security_group_id}"
 }
-
-
